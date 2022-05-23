e9bfe1a6172f0912309fb9942baf00c1e4e52025
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2022-05-23 22:51:50 +0200

    inkrementacja i dekrementacja

---
+++ wstawianie skryptu na stronę www/js/inkrementacja_dekrementacja.js
@@ -0,0 +1,23 @@
+let number9 = 0;
+
+//inkrementacja
+
+console.log(number9++);
+console.log(number9);
+
+let number10 = 2
+
+console.log(++number10); //preinkrementacja - zwiększ wartość o 1
+console.log(number10);
+
+//dekrementacja
+
+let number11 = 5;
+
+console.log(number11--);
+console.log(number11);
+
+let number12 = 8
+
+console.log(--number12); // predekrementacja - mniejsz wartość o 1
+console.log(number12);
