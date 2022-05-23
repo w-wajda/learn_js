1fa637a3d9c4325d1728322d3707bbae6a82474e
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2022-05-23 20:29:18 +0200

    null i undefinde

---
+++ wstawianie skryptu na stronę www/js/null_undefined.js
@@ -0,0 +1,12 @@
+let firstNumber;
+let secondNumber = null;
+
+console.log(firstNumber);
+console.log(secondNumber);
+
+const element = document.querySelector(".section");
+console.log(element);
+
+const element2 = document.querySelector(".section-1");
+console.log(element2);
+
