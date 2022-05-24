b23e4e8c98d1f9567bec1f5f69539cc1c5c5254f
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2022-05-24 11:20:21 +0200

    tworzenie tablic

---
+++ wstawianie skryptu na stronę www/js/tworzenie_tablic.js
@@ -0,0 +1,11 @@
+const number13 = 10;
+const numbers = [number13, 2, 5, 7, 9];
+
+console.log("Tworzenie tablicy");
+console.log(numbers.length); //zwróci ilość elementów jest w tablicy
+console.log(numbers[1]); //odwołanie się do drugiego elementu tablicy
+
+const arrays = [[2,3, 4,], [4, 2,1], 'Piotr'];
+
+console.log(arrays[0]);
+console.log(arrays[0][1]);
